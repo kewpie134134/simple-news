93ec28754eb7262397058ddd5eca53b16fe8d7cd
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-03-30 07:17:33 +0900

    ディレクトリ構成変更と、ホーム画面修正

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import MainLayout from '../layouts'
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <Head>
+        <title>Simple News</title>
+      </Head>
+    </MainLayout>
+  )
+}
