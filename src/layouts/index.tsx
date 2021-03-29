0af7a39c1cd7153b98368ee4e3b29db4cd184876
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-03-30 06:38:55 +0900

    メインのレイアウト作成

---
+++ src/layouts/index.tsx
@@ -0,0 +1,16 @@
+// 全体のレイアウトを整えるためのレイアウトコンポーネント
+import Header from '../components/header'
+import styles from './index.module.scss'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+export default function MainLayout({ children }: LayoutProps): JSX.Element {
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>{children}</main>
+    </>
+  )
+}
