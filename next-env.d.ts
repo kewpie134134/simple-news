f40f9798c0238d64df45968f4d77deba7439f8f2
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-03-30 06:45:36 +0900

    コメント追加

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+/**
+ * これは、/// <reference types="hoge" />のディレクティブを使っているので、nextとnext/types/globalの宣言をコードの中で使用することを宣言しています。
+ * これらのファイルの場所を確認してみると、node_modules/next/types/index.d.tsとnode_modules/next/types/global.d.tsにあります。
+ *これらのファイルで宣言されていることによって、Reactがグローバルで使えるようになっていたり、css modulesが使えるようになっていたりしています。
+ */
