ddbc724ff4766b6a2f633be65e0715fe05592de0
Baba Tomoyuki <bears.pigs.waterimps.48@gmail.com>
2021-03-30 22:31:11 +0900

    NewsAPI で記事を取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from 'next/head'
 import MainLayout from '../layouts'
 
-export default function Home() {
+export default function Home(props: any) {
+  // 記事を取得できているか確認
+  console.log(props.topArticles)
   return (
     <MainLayout>
       <Head>
@@ -9,4 +11,22 @@ export default function Home() {
       </Head>
     </MainLayout>
   )
+}
+
+// getStaticProps で記事を取得し、props に返り値を渡す。
+export const getStaticProps = async () => {
+  // NewsAPI のトップ記事の情報を取得する。
+  const pageSize = 10 // 取得したい記事の数
+  const topRes = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=79efafa3ff2a4f82b73922c267c269e6`,
+  )
+  const topJson = await topRes.json()
+  const topArticles = topJson?.articles
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  }
 }
